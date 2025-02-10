d415d99c4e1e42c3ae973974dd9d3882aa250383
aryadira <aryadyra27@gmail.com>
2025-02-10 20:37:06 +0700

    ADD - Test Fetch API from SERVER

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,44 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import { TableDemo } from "@/components/table-demo";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const base_url = "http://127.0.0.1:8000/api";
+  const endpoint = "user";
+  const token = "3|uI7zw1mYimbDXouVu7P6g8RHYi4vjU4udR2PLCSc21483a17";
+  const url = `${base_url}/${endpoint}`;
+
+  useEffect(() => {
+    fetch(url, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "success") {
+          setUsers(data.data);
+        } else {
+          setError(data.message);
+        }
+      })
+      .catch((err) => {
+        setError("Failed to fetch data");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -33,7 +67,7 @@ export default function Page() {
             <div className='aspect-video rounded-xl bg-muted/50' />
           </div>
           <div className='p-5 bg-white min-h-[100vh] flex-1 rounded-xl md:min-h-min'>
-            <TableDemo />
+            <TableDemo datas={users} loading={loading} />
           </div>
         </div>
       </SidebarInset>
--- src/components/table-demo.tsx
+++ src/components/table-demo.tsx
@@ -1,76 +1,50 @@
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+function filterStatus(status, type) {
+  if (status === 1) {
+    if (type === "a") return "Active";
+    if (type === "b") return "Blocked";
+    return "Disabled";
+  }
+  return "Unblocked";
+}
 
-export function TableDemo() {
+export function TableDemo({ datas, loading = false }) {
+  console.log(datas);
+
   return (
     <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableCaption>A list of your recent users.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className='w-[100px]'>Invoice</TableHead>
+          <TableHead className='w-[100px]'>No.</TableHead>
+          <TableHead>Fullname</TableHead>
+          <TableHead>Phone</TableHead>
+          <TableHead>Date of Birth</TableHead>
+          <TableHead>Gender</TableHead>
           <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className='text-right'>Amount</TableHead>
+          <TableHead>Block Status</TableHead>
+          <TableHead>Created Date</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className='font-medium'>{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className='text-right'>{invoice.totalAmount}</TableCell>
+        {datas.map((data, index) => (
+          <TableRow key={data.id}>
+            <TableCell className='font-medium'>{index + 1}</TableCell>
+            <TableCell>{data.fullname}</TableCell>
+            <TableCell>{data.phone}</TableCell>
+            <TableCell>{data.date_of_birth}</TableCell>
+            <TableCell>{data.gender}</TableCell>
+            <TableCell>{filterStatus(data.is_active, "a")}</TableCell>
+            <TableCell>{filterStatus(data.blocked, "b")}</TableCell>
+            <TableCell>{data.created_at}</TableCell>
           </TableRow>
         ))}
       </TableBody>
       <TableFooter>
         <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className='text-right'>$2,500.00</TableCell>
+          <TableCell>Total</TableCell>
+          <TableCell>{datas.length}</TableCell>
         </TableRow>
       </TableFooter>
     </Table>
